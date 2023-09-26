7c319308fc88590abcf92f0daf97b04fd1066d89
Md-Swapan <swapan@excelitai.com>
2023-09-26 14:35:10 +0600

    scroll pagination update

--- src/Pages/NewArrival/NewArrival.js
+++ src/Pages/NewArrival/NewArrival.js
@@ -6,51 +6,70 @@ import { baseUrl } from "./../../BaseUrl/BaseUrl";
 import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
 import { useRef } from "react";
 import NewArrivalProductCard from "./NewArrivalProductCard";
-
+import InfiniteScroll from "react-infinite-scroll-component";
 const NewArrival = () => {
   const [newArrivalProduct, setNewArrivalProduct] = useState([]);
   const [loading, setLoading] = useState(true);
-  const listInnerRef = useRef();
-  const [currPage, setCurrPage] = useState(1);
-  const [prevPage, setPrevPage] = useState(0);
-  const [lastList, setLastList] = useState(false);
+  // const listInnerRef = useRef();
+  // const [currPage, setCurrPage] = useState(1);
+  // const [prevPage, setPrevPage] = useState(0);
+  // const [lastList, setLastList] = useState(false);
+
+  // useEffect(() => {
+  //   let limit = 15;
+  //   const fetchData = async () => {
+  //     const response = await axios.get(
+  //       `${baseUrl}/products/latest?limit=${limit}&offset=${currPage}`
+  //     );
+  //     response && setLoading(false);
+  //     if (!response.data.products.length) {
+  //       setLastList(true);
+  //       return;
+  //     }
+  //     setPrevPage(currPage);
+  //     setNewArrivalProduct([...newArrivalProduct, ...response.data.products]);
+  //   };
+  //   if (!lastList && prevPage !== currPage) {
+  //     fetchData();
+  //   }
+  // }, [currPage, lastList, prevPage, newArrivalProduct]);
+
+  // const onScroll = () => {
+  //   if (listInnerRef.current) {
+  //     const { scrollTop, scrollHeight, clientHeight } = listInnerRef.current;
+  //     if (scrollTop + clientHeight === scrollHeight) {
+  //       setCurrPage(currPage + 1);
+  //     }
+  //   }
+  // };
+
+  const [hasMore, setHasMore] = useState(true);
+  const [page, setPage] = useState(1);
 
   useEffect(() => {
-    let limit = 15;
-    const fetchData = async () => {
-      const response = await axios.get(
-        `${baseUrl}/products/latest?limit=${limit}&offset=${currPage}`
-      );
-      response && setLoading(false);
-      if (!response.data.products.length) {
-        setLastList(true);
-        return;
-      }
-      setPrevPage(currPage);
-      setNewArrivalProduct([...newArrivalProduct, ...response.data.products]);
-    };
-    if (!lastList && prevPage !== currPage) {
-      fetchData();
-    }
-  }, [currPage, lastList, prevPage, newArrivalProduct]);
+    fetchData();
+  }, []);
 
-  const onScroll = () => {
-    if (listInnerRef.current) {
-      const { scrollTop, scrollHeight, clientHeight } = listInnerRef.current;
-      if (scrollTop + clientHeight === scrollHeight) {
-        setCurrPage(currPage + 1);
-      }
-    }
+  const fetchData = () => {
+    axios
+      .get(`${baseUrl}/products/latest?limit=${15}&offset=${page}`)
+      .then((response) => {
+        response && setLoading(false);
+        setNewArrivalProduct([...newArrivalProduct, ...response.data.products]);
+        setHasMore(response.data.products.length > 0);
+        setPage(page + 1);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
   };
-
- 
 
   return (
     <>
       <div className="newArrival_container">
         <h4>New Arrival Products:</h4>
-      
-          <div className="product-content mt-4"
+
+        {/* <div className="product-content mt-4"
            onScroll={onScroll}
            ref={listInnerRef}
            style={{ height: "100vh", overflowY: "auto" }}
@@ -80,8 +99,42 @@ const NewArrival = () => {
                 ))
               )}
             </SkeletonTheme>
+          </div> */}
+        <InfiniteScroll
+          dataLength={newArrivalProduct?.length}
+          next={fetchData}
+          hasMore={hasMore}
+          loader={<h4>Loading...</h4>}
+        >
+          <div className="product-content mt-4">
+            <SkeletonTheme baseColor="#DDDDDD" highlightColor="#e3e3e3">
+              {loading ? (
+                <>
+                  <Skeleton height="335px" borderRadius="10px" count={1} />
+                  <Skeleton height="335px" borderRadius="10px" count={1} />
+                  <Skeleton height="335px" borderRadius="10px" count={1} />
+                  <Skeleton height="335px" borderRadius="10px" count={1} />
+                  <Skeleton height="335px" borderRadius="10px" count={1} />
+                  <Skeleton height="335px" borderRadius="10px" count={1} />
+                  <Skeleton height="335px" borderRadius="10px" count={1} />
+                  <Skeleton height="335px" borderRadius="10px" count={1} />
+                  <Skeleton height="335px" borderRadius="10px" count={1} />
+                  <Skeleton height="335px" borderRadius="10px" count={1} />
+                  <Skeleton height="335px" borderRadius="10px" count={1} />
+                  <Skeleton height="335px" borderRadius="10px" count={1} />
+                  <Skeleton height="335px" borderRadius="10px" count={1} />
+                  <Skeleton height="335px" borderRadius="10px" count={1} />
+                  <Skeleton height="335px" borderRadius="10px" count={1} />
+                </>
+              ) : (
+                newArrivalProduct?.map((product) => (
+                  <NewArrivalProductCard key={product?.id} product={product} />
+                ))
+              )}
+            </SkeletonTheme>
           </div>
-        </div>
+        </InfiniteScroll>
+      </div>
     </>
   );
 };
